7102454cae4fb05874b4c16469674689bb618a61
umangshopno11 <umang.shopno@gmail.com>
2025-02-06 12:28:36 +0530

    update design

--- src/Signup.js
+++ src/Signup.js
@@ -4,64 +4,64 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 
-function Signup() { 
-    const [user, setUser] = useState("hi");
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-    const handleSubmit = async (e) => {
-        e.preventDefault(); // Prevent default form submission behavior
-    
-        const payload = {
-          email: email,
-          password: password,
-        };
-    
-        try {
-          const response = await axios.post("http://localhost:5000/user/signup", payload);
-    
-          if (response.data.status === 200) {
-            console.log(response.data.message);
-          } else {
-            console.error("Failed to process request");
-          }
-        } catch (error) {
-          console.error("Error sending request:", error);
-        }
-      };
+function Signup() {
+  const [user, setUser] = useState("hi");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
 
-      
-    return(<>
-    <form onSubmit={handleSubmit}>
+    const payload = {
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await axios.post("http://localhost:5000/user/signup", payload);
+
+      if (response.data.status === 200) {
+        console.log(response.data.message);
+      } else {
+        console.error("Failed to process request");
+      }
+    } catch (error) {
+      console.error("Error sending request:", error);
+    }
+  };
+
+
+  return (<>
+    <div class="section-container">
+      <div class="signup-card">
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
           <label>
-            Enter your email:
             <input
               type="email"
               name="email"
+              placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="p-2 m-2 rounded text-black"
               required
             />
           </label>
           <label>
-            Enter your password:
             <input
               type="password"
               name="password"
+              placeholder="Enter your password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="p-2 m-2 rounded text-black"
               required
             />
           </label>
-          <button
-            type="submit"
-            className="px-4 py-2 bg-red-500 rounded-lg font-semibold hover:bg-red-600"
-          >
-            Sign Up
-          </button>
+          <button type="submit">Sign Up</button>
         </form>
-        </>)
+      </div>
+    </div>
+
+
+  </>)
 }
 ;
 export default Signup;
